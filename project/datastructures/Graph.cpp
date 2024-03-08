1c608ca68113c1d042225c3f3f8d55a50496772e
Lara <up202208689@g.uporto.pt>
2024-03-08 16:18:44 +0000

    graph

---
+++ project/datastructures/Graph.cpp
@@ -0,0 +1,479 @@
+#include "Graph.h"
+
+/************************* Vertex  **************************/
+
+
+Vertex::Vertex(std::string in): info(in) {}
+/*
+ * Auxiliary function to add an outgoing edge to a vertex (this),
+ * with a given destination vertex (d) and edge weight (w).
+ */
+
+Edge * Vertex::addEdge(Vertex *d, double w) {
+    auto newEdge = new Edge(this, d, w);
+    adj.push_back(newEdge);
+    d->incoming.push_back(newEdge);
+    return newEdge;
+}
+
+/*
+ * Auxiliary function to remove an outgoing edge (with a given destination (d))
+ * from a vertex (this).
+ * Returns true if successful, and false if such edge does not exist.
+ */
+
+bool Vertex::removeEdge(std::string in) {
+    bool removedEdge = false;
+    auto it = adj.begin();
+    while (it != adj.end()) {
+        Edge *edge = *it;
+        Vertex *dest = edge->getDest();
+        if (dest->getInfo() == in) {
+            it = adj.erase(it);
+            deleteEdge(edge);
+            removedEdge = true; // allows for multiple edges to connect the same pair of vertices (multigraph)
+        }
+        else {
+            it++;
+        }
+    }
+    return removedEdge;
+}
+
+/*
+ * Auxiliary function to remove an outgoing edge of a vertex.
+ */
+
+void Vertex::removeOutgoingEdges() {
+    auto it = adj.begin();
+    while (it != adj.end()) {
+        Edge *edge = *it;
+        it = adj.erase(it);
+        deleteEdge(edge);
+    }
+}
+
+
+bool Vertex::operator<(Vertex & vertex) const {
+    return this->dist < vertex.dist;
+}
+
+std::string
+Vertex::getInfo() const {
+    return this->info;
+}
+
+std::vector<Edge*> Vertex::getAdj() const {
+    return this->adj;
+}
+
+bool Vertex::isVisited() const {
+    return this->visited;
+}
+
+bool Vertex::isProcessing() const {
+    return this->processing;
+}
+
+unsigned int Vertex::getIndegree() const {
+    return this->indegree;
+}
+
+double Vertex::getDist() const {
+    return this->dist;
+}
+
+Edge *Vertex::getPath() const {
+    return this->path;
+}
+
+std::vector<Edge *> Vertex::getIncoming() const {
+    return this->incoming;
+}
+
+void Vertex::setInfo(std::string in) {
+    this->info = in;
+}
+
+void Vertex::setVisited(bool visited) {
+    this->visited = visited;
+}
+
+void Vertex::setProcesssing(bool processing) {
+    this->processing = processing;
+}
+
+void Vertex::setIndegree(unsigned int indegree) {
+    this->indegree = indegree;
+}
+
+void Vertex::setDist(double dist) {
+    this->dist = dist;
+}
+
+void Vertex::setPath(Edge *path) {
+    this->path = path;
+}
+
+void Vertex::deleteEdge(Edge *edge) {
+    Vertex *dest = edge->getDest();
+    // Remove the corresponding edge from the incoming list
+    auto it = dest->incoming.begin();
+    while (it != dest->incoming.end()) {
+        if ((*it)->getOrig()->getInfo() == info) {
+            it = dest->incoming.erase(it);
+        }
+        else {
+            it++;
+        }
+    }
+    delete edge;
+}
+
+/********************** Edge  ****************************/
+
+
+Edge::Edge(Vertex *orig, Vertex *dest, double w): orig(orig), dest(dest), weight(w) {}
+
+Vertex * Edge::getDest() const {
+    return this->dest;
+}
+
+double Edge::getWeight() const {
+    return this->weight;
+}
+
+Vertex * Edge::getOrig() const {
+    return this->orig;
+}
+
+Edge *Edge::getReverse() const {
+    return this->reverse;
+}
+
+bool Edge::isSelected() const {
+    return this->selected;
+}
+
+double Edge::getFlow() const {
+    return flow;
+}
+
+void Edge::setSelected(bool selected) {
+    this->selected = selected;
+}
+
+void Edge::setReverse(Edge *reverse) {
+    this->reverse = reverse;
+}
+
+void Edge::setFlow(double flow) {
+    this->flow = flow;
+}
+
+/********************** Graph  ****************************/
+
+
+int Graph::getNumVertex() const {
+    return vertexSet.size();
+}
+
+std::unordered_map<std::string, Vertex*> Graph::getVertexSet() const {
+    return vertexSet;
+}
+
+/*
+ * Auxiliary function to find a vertex with a given content.
+ */
+Vertex* Graph::findVertex(const std::string&in) const {
+    return vertexSet.at(in);
+}
+
+
+/*
+ *  Adds a vertex with a given content or info (in) to a graph (this).
+ *  Returns true if successful, and false if a vertex with that content already exists.
+ */
+bool Graph::addVertex(const std::string &in) {
+    vertexSet.emplace(in, new Vertex(in));
+    return true;
+}
+
+/*
+ *  Removes a vertex with a given content (in) from a graph (this), and
+ *  all outgoing and incoming edges.
+ *  Returns true if successful, and false if such vertex does not exist.
+ */
+
+bool Graph::removeVertex(const std::string& in) {
+    auto it = vertexSet.find(in);
+    if (it != vertexSet.end()) {
+        Vertex* v = it->second;
+        v->removeOutgoingEdges();
+        for (auto u: vertexSet) {
+            (u.second)->removeEdge(v->getInfo());
+        }
+        vertexSet.erase(it);
+        delete v;
+        return true;
+    }
+    return false;
+}
+
+/*
+ * Adds an edge to a graph (this), given the contents of the source and
+ * destination vertices and the edge weight (w).
+ * Returns true if successful, and false if the source or destination vertex does not exist.
+ */
+
+bool Graph::addEdge(const std::string &sourc, const std::string& dest, double w) {
+    auto v1 = findVertex(sourc);
+    auto v2 = findVertex(dest);
+    if (v1 == nullptr || v2 == nullptr)
+        return false;
+    v1->addEdge(v2, w);
+    return true;
+}
+
+/*
+ * Removes an edge from a graph (this).
+ * std::string
+ * he edge is identified by the source (sourc) and destination (dest) contents.
+ * Returns true if successful, and false if such edge does not exist.
+ */
+bool Graph::removeEdge(const std::string& sourc, const std::string& dest) {
+    Vertex * srcVertex = findVertex(sourc);
+    if (srcVertex == nullptr) {
+        return false;
+    }
+    return srcVertex->removeEdge(dest);
+}
+
+bool Graph::addBidirectionalEdge(const std::string& sourc, const std::string& dest, double w) {
+    auto v1 = findVertex(sourc);
+    auto v2 = findVertex(dest);
+    if (v1 == nullptr || v2 == nullptr)
+        return false;
+    auto e1 = v1->addEdge(v2, w);
+    auto e2 = v2->addEdge(v1, w);
+    e1->setReverse(e2);
+    e2->setReverse(e1);
+    return true;
+}
+
+/****************** DFS ********************/
+
+/*
+ * Performs a depth-first search (dfs) traversal in a graph (this).
+ * Returns a vector with the contents of the vertices by dfs order.
+ */
+
+std::vector<std::string> Graph::dfs() const {
+    std::vector<std::string> res;
+    for (auto v : vertexSet)
+        v.second->setVisited(false);
+    for (auto v : vertexSet)
+        if (!v.second->isVisited())
+            dfsVisit(v.second, res);
+    return res;
+}
+
+/*
+ * Performs a depth-first search (dfs) in a graph (this) from the source node.
+ * Returns a vector with the contents of the vertices by dfs order.
+ */
+
+std::vector<std::string> Graph::dfs(const std::string& source) const {
+    std::vector<std::string> res;
+    // Get the source vertex
+    auto s = findVertex(source);
+    if (s == nullptr) {
+        return res;
+    }
+    // Set that no vertex has been visited yet
+    for (auto v : vertexSet) {
+        v.second->setVisited(false);
+    }
+    // Perform the actual DFS using recursion
+    dfsVisit(s, res);
+
+    return res;
+}
+
+/*
+ * Auxiliary function that visits a vertex (v) and its adjacent, recursively.
+ * Updates a parameter with the list of visited node contents.
+ */
+
+void Graph::dfsVisit(Vertex *v, std::vector<std::string> & res) const {
+    v->setVisited(true);
+    res.push_back(v->getInfo());
+    for (auto & e : v->getAdj()) {
+        auto w = e->getDest();
+        if (!w->isVisited()) {
+            dfsVisit(w, res);
+        }
+    }
+}
+
+/****************** BFS ********************/
+/*
+ * Performs a breadth-first search (bfs) in a graph (this), starting
+ * from the vertex with the given source contents (source).
+ * Returns a vector with the contents of the vertices by bfs order.
+ */
+
+std::vector<std::string> Graph::bfs(const std::string
+                                    & source) const {
+    std::vector<std::string> res;
+    // Get the source vertex
+    auto s = findVertex(source);
+    if (s == nullptr) {
+        return res;
+    }
+
+    // Set that no vertex has been visited yet
+    for (auto v : vertexSet) {
+        v.second->setVisited(false);
+    }
+
+    // Perform the actual BFS using a queue
+    std::queue<Vertex *> q;
+    q.push(s);
+    s->setVisited(true);
+    while (!q.empty()) {
+        auto v = q.front();
+        q.pop();
+        res.push_back(v->getInfo());
+        for (auto & e : v->getAdj()) {
+            auto w = e->getDest();
+            if ( ! w->isVisited()) {
+                q.push(w);
+                w->setVisited(true);
+            }
+        }
+    }
+    return res;
+}
+
+/****************** isDAG  ********************/
+/*
+ * Performs a depth-first search in a graph (this), to determine if the graph
+ * is acyclic (acyclic directed graph or DAG).
+ * During the search, a cycle is found if an edge connects to a vertex
+ * that is being processed in the stack of recursive calls (see theoretical classes).
+ * Returns true if the graph is acyclic, and false otherwise.
+ */
+
+bool Graph::isDAG() const {
+    for (auto v : vertexSet) {
+        v.second->setVisited(false);
+        v.second->setProcesssing(false);
+    }
+    for (auto v : vertexSet) {
+        if (! v.second->isVisited()) {
+            if ( ! dfsIsDAG(v.second) ) return false;
+        }
+    }
+    return true;
+}
+
+/**
+ * Auxiliary function that visits a vertex (v) and its adjacent, recursively.
+ * Returns false (not acyclic) if an edge to a vertex in the stack is found.
+ */
+
+bool Graph::dfsIsDAG(Vertex *v) const {
+    v->setVisited(true);
+    v->setProcesssing(true);
+    for (auto e : v->getAdj()) {
+        auto w = e->getDest();
+        if (w->isProcessing()) return false;
+        if (! w->isVisited()) {
+            if (! dfsIsDAG(w)) return false;
+        }
+    }
+    v->setProcesssing(false);
+    return true;
+}
+
+/****************** toposort ********************/
+//=============================================================================
+// Exercise 1: std::string
+// opological Sorting
+//=============================================================================
+// std::string
+// ODO
+/*
+ * Performs a topological sorting of the vertices of a graph (this).
+ * Returns a vector with the contents of the vertices by topological order.
+ * If the graph has cycles, returns an empty vector.
+ * Follows the algorithm described in theoretical classes.
+ */
+
+
+std::vector<std::string> Graph::topsort() const {
+    std::vector<std::string> res;
+
+    for (auto v : vertexSet) {
+        v.second->setIndegree(0);
+    }
+    for (auto v : vertexSet) {
+        for (auto e : v.second->getAdj()) {
+            unsigned int indegree = e->getDest()->getIndegree();
+            e->getDest()->setIndegree(indegree + 1);
+        }
+    }
+
+    std::queue<Vertex *> q;
+    for (auto v : vertexSet) {
+        if (v.second->getIndegree() == 0) {
+            q.push(v.second);
+        }
+    }
+
+    while( !q.empty() ) {
+        Vertex * v = q.front();
+        q.pop();
+        res.push_back(v->getInfo());
+        for(auto e : v->getAdj()) {
+            auto w = e->getDest();
+            w->setIndegree(w->getIndegree() - 1);
+            if(w->getIndegree() == 0) {
+                q.push(w);
+            }
+        }
+    }
+
+    if ( res.size() != vertexSet.size() ) {
+        //std::cout << "Impossible topological ordering!" << std::endl;
+        res.clear();
+        return res;
+    }
+
+    return res;
+}
+
+inline void deleteMatrix(int **m, int n) {
+    if (m != nullptr) {
+        for (int i = 0; i < n; i++)
+            if (m[i] != nullptr)
+                delete [] m[i];
+        delete [] m;
+    }
+}
+
+inline void deleteMatrix(double **m, int n) {
+    if (m != nullptr) {
+        for (int i = 0; i < n; i++)
+            if (m[i] != nullptr)
+                delete [] m[i];
+        delete [] m;
+    }
+}
+
+Graph::~Graph() {
+    deleteMatrix(distMatrix, vertexSet.size());
+    deleteMatrix(pathMatrix, vertexSet.size());
+}
+
