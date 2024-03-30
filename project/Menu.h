9b610058823f08730dec792fed584495c74f7326
Bruno <up202205619@up.pt>
2024-03-30 18:31:01 +0000

    Improved and updated doxygen

--- project/Menu.h
+++ project/Menu.h
@@ -11,8 +11,31 @@
  *
  * \section utility_sec What can this project do?
  *
+ * Choose to import from the original dataset (big or small) or a custom dataset from file paths.
 
- * Change settings such enabling colour mode.
+ * Get information such as:
+ * - List of all City in the dataset and there respective information;
+ * - Get the information of a specific City from a given code or name or ID;
+ * - Get the information of the City that has max or min demand;
+ * - Get the information of the City that has max or min population;
+ * - List of all Reservoir in the dataset and there respective information;
+ * - Get the information of a specific Reservoir from a given code or name or ID or Municipality;
+ * - Get the information of the Reservoir that has max or min delivery;
+ * - List of all Station in the dataset and there respective information;
+ * - Get the statistics of the network such as Average (Capacity - Flow), Max (Capacity - Flow), Variance (Capacity - Flow), current flow to all City and current max flow.
+
+ * Make requests such as:
+ * - Compute the standard max flow of the network;
+ * - Compute the max flow of the network but prioritizing specific City given by the user or in order of population;
+ * - Compute the max flow of the network with overflow;
+ * - Compute the max flow of the network with overflow that is able to reach a specific City;
+ * - Compute the max flow of the network with overflow for only a given list of City;
+ * - Erase specific Reservoir, Station or pipes from the Graph;
+ * - Balance the flow of the network in order to reduce the Variance (Capacity - Flow).
+
+ * Change settings such as:
+ * - Enabling colour mode;
+ * - Changing the display mode of the network statistics.
  */
 
 #include "WaterSupply.h"
